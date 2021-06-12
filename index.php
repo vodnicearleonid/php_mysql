f3f0640c694dfca1b6dd670a03d9dfb8fef95413
vodnicearleonid <vodnicear@gmail.com>
2021-06-12 21:57:04 +0300

    index.php Уроки PHP для начинающих _ #5 – Строковые операции

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>l2php</title>
+    <title>Уроки PHP для начинающих 5 – Строковые операции</title>
 </head>
 <body>
 <?php
