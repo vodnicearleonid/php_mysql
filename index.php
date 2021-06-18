6e47680d87fcbc4fbd38a157f9b80dd6cace17c7
vodnicearleonid <vodnicear@gmail.com>
2021-06-18 18:13:18 +0300

    Уроки PHP 15 – Работа с файлами
    Signed-off-by: vodnicearleonid <vodnicear@gmail.com>

--- index.php
+++ index.php
@@ -5,100 +5,56 @@ require "blocks/header.php";
 <h3>Pagina principala</h3>
 
 <?php
+//$file = fopen("text.txt", "w");
+//fwrite($file, "Exemplu de text scris in fisier prin limbajul de programare php");
+//fclose($file);
+//
+//echo '<br>';
+//echo '<br>';
 
-$list = [5,7,3,6,7,8];
-//unset($list[1]); // stergem un element din masiv
-//sort($list); //sortam un masiv de la mic la mare
-//rsort($list); //sortam un masiv invers de la mare la mic
-//shuffle($list); //sortam un masiv aleatoriu
-//echo in_array(3, $list);// cauta un element in masiv, 1true, numic false rezultat 1Array ( [0] => 6 [1] => 8 [2] => 7 [3] => 3 [4] => 5 )
+//$file = fopen("text.txt", "a");
+//fwrite($file, "\nExemplu de adaugate text in fisier la cel existent");
+//fclose($file);
+//
+//echo '<br>';
+//echo '<br>';
 
-//if (in_array(3, $list) == ""){
-//    echo "not found";
-//}else{
-//    echo "found";
-//}
+//$filename = "text.txt";
+//$file = fopen($filename, "r");
+//    $content = fread($file, filesize($filename));
+//fclose($file);
+//echo "<pre>" . $content . "</pre>";
+//
+//echo '<br>'; // test content file, test2, test3
 
-$arr = array_slice($list, 0, count($list)); // array(6) { [0]=> int(5) [1]=> int(7) [2]=> int(3) [3]=> int(6) [4]=> int(7) [5]=> int(8) }
-var_dump($arr);
+//$filename = "text.txt";
+//$file = fopen($filename, "r");
+//$content = fread($file, filesize($filename));
+//fclose($file);
+//echo "<pre>" . $content . "</pre>";
 
-echo '<br>';
-echo '<br>';
+//echo '<br>'; // test content file, test2, test3
+//file_put_contents("text.txt", "text exemlu");//adauga test in fisierul creat sau existent
+//file_get_contents("a.txt");// afiseaza continutul text din fisier
 
-$arr1 = [5, 7];
-$arr2 = [6, 8, 9];
-$arr3 = array_merge($arr1, $arr2); //lipeste mai multe masive Array ( [0] => 5 [1] => 7 [2] => 3 [3] => 6 [4] => 7 [5] => 8 )
-print_r($arr3);
+//echo "<br>";
+//echo file_exists("a.txt");// 1
 
-echo '<br>';
-echo '<br>';
+//echo "<br>";
+//rename("a.txt", "new.txt");//redenumeste fisierul a.txt in new.php
 
+//echo "<br>";
+//unlink("test.php");//sterge complet fisierul
 
+echo __FILE__;// /Users/leovodnicear/Documents/PHP_MySQL/index.php drumul fisierului
 
-print_r($list);
-
-echo '<br>';
-echo '<br>';
-
-$x = 10;
-echo gettype($x); //integer
-echo '<br>';
-echo '<br>';
-
-$x = "10";
-echo gettype($x); //string
-echo '<br>';
-echo '<br>';
-
-$x = floatval("10");
-echo gettype($x); //double
-echo is_numeric($x)."<br>";//double1
-echo '<br>';
-echo '<br>';
+echo fileperms(__FILE__);
 
 
-$x = "10";
-echo  is_integer($x)."<br>";//false
-echo '<br>';
-echo '<br>';
 
-$x = "10";
-echo  is_double($x)."<br>";//false
-echo '<br>';
-echo '<br>';
 
-$x = "10";
-echo  is_string($x)."<br>";//1
-echo '<br>';
-echo '<br>';
 
-$x = "10";
-echo  is_numeric($x)."<br>";//1
-echo '<br>';
-echo '<br>';
 
-$x = "10";
-echo  is_bool($x)."<br>";//false
-echo '<br>';
-echo '<br>';
-
-$x = [];
-echo  is_array($x)."<br>";//1 true
-echo '<br>';
-echo '<br>';
-
-$str = "Exemplu";
-echo  strpos($str, "mp"); // 3 transforma in masiv din trei elemente
-echo '<br>';
-echo '<br>';
-
-$words = "jhon, bob, alex";
-$arr_words = explode(",", $words); // Array ( [0] => jhon [1] => bob [2] => alex )
-print_r($arr_words);
-echo "<br>" . implode(" | ", $arr_words); //jhon | bob | alex
-
-echo '<br>';
-echo '<br>';
 require "blocks/footer.php";
 ?>
 
