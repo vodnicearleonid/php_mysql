a3cef1a7645a02e506eff5e3e09383c347b73385
vodnicearleonid <vodnicear@gmail.com>
2021-06-23 12:13:20 +0300

    SESSION functional state PHP 19 – Форма обратной связи
    Signed-off-by: vodnicearleonid <vodnicear@gmail.com>

--- contacts.php
+++ contacts.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-echo "<pre>";
-print_r($_SESSION);
-echo "</pre>";
+//session_destroy();
+//echo "<pre>";
+//print_r($_SESSION);
+//echo "</pre>";
 
 $title = "Contacts";
 require "blocks/header.php";
@@ -10,21 +11,40 @@ require "blocks/header.php";
     <h2 class="mt-5"><?php $title ?></h2>
 
     <h6>Contacts</h6><br>
-
-    <div class="text-success"><?=$_SESSION['success_mail']?></div>
+<?php
+if (isset($_SESSION['success_mail'])) {
+    echo '<div class="text-success">' . $_SESSION['success_mail'] . '</div>';
+}
+?>
 
     <form action="check_contact.php" method="post">
-        <input type="text" name="username" value="<?=$_SESSION['username']?>" placeholder="My Username" class="form-control">
-        <div class="text-danger"><?=$_SESSION['error_username']?></div><br>
+        <input type="text" name="username" value="<?=$_SESSION['username']?>" placeholder="My Username" class="form-control"><br>
+        <?php
+            if (isset($_SESSION['error_username'])){
+               echo '<div class="text-danger">' . $_SESSION['error_username'] . '</div><br>';
+            }
+        ?>
 
-        <input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="My Email" class="form-control">
-        <div class="text-danger"><?=$_SESSION['error_email']?></div><br>
+        <input type="email" name="email" value="<?=$_SESSION['email']?>" placeholder="My Email" class="form-control"><br>
+        <?php
+        if (isset($_SESSION['error_email'])){
+            echo '<div class="text-danger">' . $_SESSION['error_email'] . '</div><br>';
+        }
+        ?>
 
-        <input type="text" name="subject" value="<?=$_SESSION['subject']?>" placeholder="My Subject" class="form-control">
-        <div class="text-danger"><?=$_SESSION['error_subject']?></div><br>
+        <input type="text" name="subject" value="<?=$_SESSION['subject']?>" placeholder="My Subject" class="form-control"><br>
+        <?php
+        if (isset($_SESSION['error_subject'])){
+            echo '<div class="text-danger">' . $_SESSION['error_subject'] . '</div><br>';
+        }
+        ?>
 
-        <textarea name="message" placeholder="My Message" class="form-control"><?=$_SESSION['message']?></textarea>
-        <div class="text-danger"><?=$_SESSION['error_message']?></div><br>
+        <textarea name="message" placeholder="My Message" class="form-control"><?=$_SESSION['message']?></textarea><br>
+        <?php
+        if (isset($_SESSION['error_message'])){
+            echo '<div class="text-danger">' . $_SESSION['error_message'] . '</div><br>';
+        }
+        ?>
 
         <input class="btn btn-primary" type="submit" value="send!" >
     </form>
